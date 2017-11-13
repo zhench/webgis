5a279f9722196a207509e7ba9c2067fca7cc7f4e
Shugui <shuguid@hotmail.com>
2017-11-14 01:12:39 +0800

    homework

--- widgets/MoveableWidgetFrame.js
+++ widgets/MoveableWidgetFrame.js
@@ -1,7 +1,7 @@
 define([
         "dojo/_base/declare", "dojo/_base/array", "dojo/query", "dojo/dom-style", "dojo/on", "dojo/fx", "dojo/_base/fx", "dojo/_base/lang", "dojo/dom-attr", "dojo/dom-class", "dojo/dom-geomety", "dijit/_Widget", "dijit/_Container", "dojo/dnd/Moverable", "dijit/_TemplatedMixin", "dojo/text!./templates/MoveableWidgetFrame.html"
     ],
-    function(declare, array, query, domStyle, on, coreFx, fx, lang, domClass, domGeom, _Widget, _Container, Moverable, _TemplatedMixin, template) {
+    function(declare, array, query, domStyle, on, coreFx, fx, lang, domAttr, domClass, domGeom, _Widget, _Container, Moverable, _TemplatedMixin, template) {
         return declare([_Widget, _TemplatedMixin, _Container], {
             widget: null,
             icon: "",
@@ -100,8 +100,102 @@ define([
                     this.titleNode.innerHTML = this.title;
 
                     var minBtn = query(".wbMinimize", this.domNode)[0];
+                    minBtnTd = minBtn.parentNode;
+                    if (widget.panels.length > 1) {
+                        array.forEach(widget.panels, lang.hitch(this, function(item, idx, arr) {
+                            var td = document.createElement("TD");
+                            var btn = document.createElement("DIV");
+                            domClass.add(btn, "widgetButton");
+                            domStyle.set(btn, "backgroundImage", "url(" + require.toUrl("webgis2book/widgets/" + item.buttonIcon) + ")");
+                            domAttr.set(btn, "title", item.buttonText);
+                            if (this.state === "minimized") {
+                                domStyle.set(btn, "display", "none");
+                            }
+                            td.appendChild(btn);
+                            minBtnTd.parentNode.insertBefore(td, minBtnTd);
+                            on(btn, "click", lang.hitch(this, function() {
+                                this.selectPanel(idx);
+                            }));
+                        }))
+                    }
                 } catch (err) {
+                    console.error(err);
+                }
+            },
+            maximize: function(duration) {
+                var boxEndProperties = {
+                    height: this.boxMaximized.h[this.widget.panelIndex],
+                    paddingTop: this.boxMaximized.paddingTop,
+                    marginTop: 0,
+                    marginLeft: this.boxMaximized.marginLeft,
+                    width: this.boxMaximized.w,
+                    paddingLeft: this.boxMaximized.paddingLeft,
+                    paddingRight: this.boxMaximized.paddingRight
+                };
+                var badgeEndProperties = { left: 0 };
+                if (duration !== 0 && !duration) {
+                    duration = 350;
+                }
+                if (duration <= 0) {
+                    //不使用动画效果
+                    for (var key in boxEndProperties) {
+                        boxEndProperties[key] = boxEndProperties[key] + "px";
+                    }
+                    for (var key in badgeEndProperties) {
+                        badgeEndProperties[key] = badgeEndProperties[key] + "px";
+                    }
+                    domStyle.set(this.badgeNode, badgeEndProperties);
+                    domStyle.set(this.boxNode, boxEndProperties);
+                    domStyle.set(this.contentNode, "overflow", "auto");
+                    query(".widgetButton", this.domNode).style("display", "block");
+                    this.statue = "maximized";
+                } else {
+                    try {
+                        var badgeSlider = fx.animateProperty({ node: this.badgeNode, properties: badgeEndProperties });
+                        var hShrink = fx.animateProperty({
+                            node: this.boxNox,
+                            properties: {
+                                marginLeft: 0,
+                                width: 10,
+                                paddingLeft: 0,
+                                paddingRight: 0
+                            },
+                            onEnd: lang.hitch(this, function() {
+                                var hGrow = fx.animateProperty({
+                                    node: this.boxNode,
+                                    properties: {
+                                        width: boxEndProperties.width,
+                                        paddingLeft: boxEndProperties.paddingLeft,
+                                        paddingRight: boxEndProperties.paddingRight,
+                                        marginLeft: boxEndProperties.marginLeft
+                                    }
+                                });
+                                var vGrow = fx.animateProperty({
+                                    node: this.boxNode,
+                                    beforeBegin: lang.hitch(this, function() {
+                                        domStyle.set(this.contentNode, "display", "block");
+                                    }),
+                                    onEnd: lang.hitch(this, function() {
+                                        this.state = "maximized";
+                                        domStyle.set(this.contentNode, "display", "block");
+                                        query(".widgetButton", this.domNode).style("display", "block");
+                                    }),
+                                    properties: {
+                                        height: boxEndProperties.height,
+                                        paddingTop: boxEndProperties.paddingTop,
+                                        paddingBottom: boxEndProperties.paddingBottom,
+                                        marginTop: boxEndProperties.marginTop
+                                    }
+                                });
+                                coreFx.chain([hGrow, vGrow]).play();
+                            })
+                        })
+                        cor.combine([badgeSlider, hShrink]).play();
+                        thiss.state = "maximizing";
+                    } catch (err) {
+                        console.error(drr);
 
+                    }
                 }
             }
         })
